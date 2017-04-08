f47756a885dfac75893b41f9ca0bfa73461fe293
John Kirkley <johnkirkley@s172-20-39-h171.paws.uga.edu>
2017-04-08 11:13:12 -0400

    intro page start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { Intro } from '../pages/intro/intro';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'intro', component: Intro }
     ];
 
   }
